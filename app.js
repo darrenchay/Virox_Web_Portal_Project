09660ff45bba3473fda6f5fce1d3df649ca275d0
darrenchay <darrenchay@gmail.com>
2020-06-16 15:32:11 -0400

    created basic api functionality for database management

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-const { app, BrowserWindow } = require('electron');
+//Electron
+const { BrowserWindow } = require('electron');
+const { app } = require('electron');
+const Window = require('./Window');
+
 const sqlite3 = require('sqlite3').verbose();
-const axios = require('axios');
-const fs = require('fs');
-const path    = require("path");
-const express = require('./express'); 
 
 
-/****************** SQL SECTION ******************/
+
+function main() {
+  let mainWindow = new Window({
+    file: 'public/index.html'
+  });
+}
+
+app.on('ready', main());
+
+app.on('window-all-closed', function () {
+  app.quit();
+});
+
+
 
 /************************* POST/GET REQUESTS *************************/
-app.get('/getData', function(req, res) {
+/* app.get('/getData', function(req, res) {
 
-  let db = new sqlite3.Database('ViroxDB.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log("Connected to the database");
-  });
   
-  let sql = 'SELECT * FROM `EXPERIMENT_RECORDS`;'
-  
-  db.all(sql, [], (err, rows) => {
-  if (err) {
-    throw err;
-  }
-
-  rows.forEach((row) => {
-    console.log(row.project_title);
-  });
-  });
-  
-  db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-  });
 
   res.send({
     row: rows
@@ -43,25 +32,31 @@ app.get('/getData', function(req, res) {
   
 
 });
+ */
 
+/* let db = new sqlite3.Database('ViroxDB.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+      return console.error(err.message);
+  }
+  console.log("Connected to the database");
+});
 
-/************************* ELECTRON STARTUP *************************/
-function createWindow () {
-  express();
-  // Create the browser window.
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
+let sql = 'SELECT * FROM `EXPERIMENT_RECORDS`;'
 
-  // and load the index.html of the app.
-  win.loadFile('public/index.html');
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
+db.all(sql, [], (err, rows) => {
+if (err) {
+  throw err;
 }
 
-app.whenReady().then(createWindow);
+rows.forEach((row) => {
+  console.log(row.project_title);
+});
+});
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+}); */
+
