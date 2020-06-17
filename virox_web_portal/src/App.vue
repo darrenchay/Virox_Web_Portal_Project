04d8c273405337dd1033648056494d286b5df072
darrenchay <darrenchay@gmail.com>
2020-06-17 16:33:04 -0400

    cleaning repo

--- virox_web_portal/src/App.vue
+++ virox_web_portal/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="card">
+    <ExperimentForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import ExperimentForm from './components/ExperimentForm.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ExperimentForm
   }
 }
 </script>
--- virox_web_portal/src/main.js
+++ virox_web_portal/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import { store } from './store.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
+})
