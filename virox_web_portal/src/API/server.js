be21dac48bd0a9cdfd3963b89fdcae1ca82edadd
darrenchay <darrenchay@gmail.com>
2020-06-23 19:16:14 -0400

    started working on server communication with database

---
+++ virox_web_portal/src/API/server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const sqlite3 = require('sqlite3').verbose();
+
+// open the database
+let db = new sqlite3.Database('./ViroxDB.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the chinook database.');
+});
+
+db.serialize(() => {
+  db.each(`SELECT PlaylistId as id,
+                  Name as name
+           FROM playlists`, (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(row.id + "\t" + row.name);
+  });
+});
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
+
+const records = [
+    {id: 1, name: 'User 1'},
+    {id: 2, name: 'User 2'},
+    {id: 3, name: 'User 3'},
+    {id: 4, name: 'User 4'},
+    {id: 5, name: 'User 5'},
+    {id: 6, name: 'User 6'},
+    {id: 7, name: 'User 7'},
+    {id: 8, name: 'User 8'},
+    {id: 9, name: 'User 9'},
+    {id: 10, name: 'User 10'}
+]
+
+app.get('/users', (req, res) => {
+    const page = req.query.page
+    const limit = req.query.limit
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    let results = records.slice(startIndex, endIndex)
+    res.json(results)
+})
+
+app.listen(3000)
