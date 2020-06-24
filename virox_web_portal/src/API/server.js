8e650493ec2435e8a2a5313ed313352e6fa2a607
darrenchay <darrenchay@gmail.com>
2020-06-23 20:31:51 -0400

    created a get request for the API

--- virox_web_portal/src/API/server.js
+++ virox_web_portal/src/API/server.js
@@ -2,44 +2,39 @@ const express = require('express')
 const app = express()
 const sqlite3 = require('sqlite3').verbose();
 
-// open the database
-let db = new sqlite3.Database('./ViroxDB.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the chinook database.');
-});
 
-db.serialize(() => {
-  db.each(`SELECT PlaylistId as id,
-                  Name as name
-           FROM playlists`, (err, row) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log(row.id + "\t" + row.name);
-  });
-});
+app.get('/getRecords', (req, res) => {
+    var sql = "SELECT * FROM EXPERIMENT_RECORDS ORDER BY record_id"
+    var params = []
 
-db.close((err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+    // open the database
+    const db = new sqlite3.Database('./ViroxDB.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Connected to the virox database.');
+    });
 
-const records = [
-    {id: 1, name: 'User 1'},
-    {id: 2, name: 'User 2'},
-    {id: 3, name: 'User 3'},
-    {id: 4, name: 'User 4'},
-    {id: 5, name: 'User 5'},
-    {id: 6, name: 'User 6'},
-    {id: 7, name: 'User 7'},
-    {id: 8, name: 'User 8'},
-    {id: 9, name: 'User 9'},
-    {id: 10, name: 'User 10'}
-]
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+
+
+})
 
 app.get('/users', (req, res) => {
     const page = req.query.page
@@ -52,4 +47,4 @@ app.get('/users', (req, res) => {
     res.json(results)
 })
 
-app.listen(3000)
+app.listen(8080)
