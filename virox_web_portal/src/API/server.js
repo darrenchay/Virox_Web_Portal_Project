d5cb9ee078b64e63bbf8aad5ed6008bea8c30f03
darrenchay <darrenchay@gmail.com>
2020-06-23 21:22:47 -0400

    added fetching of a single record by record id

--- virox_web_portal/src/API/server.js
+++ virox_web_portal/src/API/server.js
@@ -1,45 +1,65 @@
 const express = require('express')
-const app = express()
+const router = express.Router()
 const sqlite3 = require('sqlite3').verbose();
+const app = express()
 
+app.use("/", router)
 
-app.get('/getRecords', (req, res) => {
+//Get all the records to display on table
+router.get('/getRecords', (req, res) => {
     var sql = "SELECT * FROM EXPERIMENT_RECORDS ORDER BY record_id"
     var params = []
 
-    // open the database
-    const db = new sqlite3.Database('./ViroxDB.db', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Connected to the virox database.');
-    });
+    const db = openDB()
 
     db.all(sql, params, (err, rows) => {
         if (err) {
-            res.status(400).json({ "error": err.message });
+            res.status(400).send({ "error": err.message });
             return;
         }
-        res.json({
-            "message": "success",
+        res.send({
+            "message": "successfully retrieved " + rows.length + " records",
             "data": rows
         })
     });
 
-    db.close((err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Close the database connection.');
-    });
-
+    closeDB(db)
 
 })
 
-app.get('/users', (req, res) => {
+//Get a single complete record data 
+router.get('/getRecord', (req, res) => {
+    const id = req.query.id
+    console.log(id)
+    
+    const db = openDB()
+
+    const sql = 'SELECT * FROM EXPERIMENT_RECORDS WHERE record_id = ?'
+
+    db.all(sql, id, (err, row) => {
+        //Process if sql statement gives an error
+        if (err) {
+            res.status(500).send({ "Error ": err.message })
+            return
+        } else if (row.length > 0){
+            res.send({
+                "Message": "Successfully retrieved " + row.length + " row",
+                data: row
+            })
+        } else {
+            res.send("ERROR: Record with ID " + id + " not retrieved")
+        }
+    })
+
+    closeDB(db)    
+})
+
+//
+
+router.get('/users', (req, res) => {
     const page = req.query.page
     const limit = req.query.limit
-
+    console.log(page)
     const startIndex = (page - 1) * limit
     const endIndex = page * limit
 
@@ -47,4 +67,25 @@ app.get('/users', (req, res) => {
     res.json(results)
 })
 
-app.listen(8080)
+app.listen(3000)
+
+function openDB() {
+    // open the database
+    const db = new sqlite3.Database('./ViroxDB.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            console.error(err.message)
+        }
+        console.log('Connected to the virox database.')
+    })
+
+    return db
+}
+
+function closeDB (db) {
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Close the database connection.')
+    })
+}
