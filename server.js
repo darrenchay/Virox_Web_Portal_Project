aba92b1d9ee7b23226bf4b14f2a56ee83d618b69
darrenchay <darrenchay@gmail.com>
2020-07-06 14:03:39 -0400

    moved server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path')
 const app = express()
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/client', serveStatic(path.join(__dirname, '/dist')))
 
 // this * route is to serve project on different page routes except root `/`
-app.get('/*', function (req, res) {
+app.get('/client/*', function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
