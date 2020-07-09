c1d61865e70002919d111d590fff09a53470bb7b
darrenchay <darrenchay@gmail.com>
2020-07-09 02:03:53 -0400

    GOT BACKEND AND FRONEND WORKING

--- client/src/components/RecordDetails.vue
+++ client/src/components/RecordDetails.vue
@@ -268,8 +268,8 @@
 <script>
 const axios = require("axios");
 
-//const baseURL = "/API";
-const baseURL = "http://localhost:3000/API"
+const baseURL = "https://virox-server.herokuapp.com/api";
+//const baseURL = "http://localhost:3000/API"
 export default {
   name: "RecordDetails",
   template: "#general-input-form",
--- server/dbAuth.js
+++ server/dbAuth.js
@@ -7,7 +7,7 @@ const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PAS
 console.log(connectionString);
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
